36fb57e49020ea1df0c357f1756b4453782e97f3
Xen0mrefne <x3n0g3n1c@gmail.com>
2023-01-08 17:23:04 -0300

    task mock and interface

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text:"Eat chocolate",
+        day: "May 6, 12:00 AM",
+        reminder: true
+    },
+    {
+        id: 2,
+        text:"Eat chococrispis",
+        day: "Aug 17, 18:00 AM",
+        reminder: false
+    },
+    {
+        id: 3,
+        text:"Eat chocolate aguila",
+        day: "Sep 23, 15:30 AM",
+        reminder: true
+    },
+]
